2ac07f2e2d30f0350c72fff1a3d563b787ab79f9
oswaldomauricio <37278259+oswaldomauricio@users.noreply.github.com>
2023-04-23 22:51:58 -0400

    fix-card-pronto
    animação e interação do usuario pronta.

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,25 @@
-function projetos(){
-    alert("ainda não disponivel, aguarde o fim do portfolio.")
+let card_design = document.getElementById("card-design")
+let card_desenvolvimento_web = document.getElementById("desenvolvimento-web-card")
+let excel_card = document.getElementById("excel-card")
+
+let card_servicos_design = document.getElementById("card-design")
+
+
+function desenvolvimento_web(){
+    card_desenvolvimento_web.style.display = "grid";
+    card_servicos_design.style.display = "none";
+    excel_card.style.display = "none";
+
+
+}
+function designer(){
+    card_desenvolvimento_web.style.display = "none";
+    card_servicos_design.style.display = "grid";
+    excel_card.style.display = "none";
+
+}
+function excel(){
+    card_desenvolvimento_web.style.display = "none";
+    card_servicos_design.style.display = "none";
+    excel_card.style.display = "grid";
 }
