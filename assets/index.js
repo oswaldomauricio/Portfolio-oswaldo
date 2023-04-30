b0bf80d881e4ef85800de93a786391ea89e87e04
oswaldomauricio <37278259+oswaldomauricio@users.noreply.github.com>
2023-04-30 14:41:59 -0400

    alterações na aba sobre
    enviando alterações no sobre

--- assets/index.js
+++ assets/index.js
@@ -5,6 +5,15 @@ let excel_card = document.getElementById("excel-card")
 let card_servicos_design = document.getElementById("card-design")
 
 
+let btns_competencias_designer = document.getElementById('designer')
+let btns_competencias_desenvolvimento_web = document.getElementById('desenvolvimento-web')
+let btns_competencias_excel = document.getElementById('excel')
+
+btns_competencias_designer.addEventListener('click', designer)
+btns_competencias_desenvolvimento_web.addEventListener('click', desenvolvimento_web)
+btns_competencias_excel.addEventListener('click', excel)
+
+
 function desenvolvimento_web(){
     card_desenvolvimento_web.style.display = "grid";
     card_servicos_design.style.display = "none";
